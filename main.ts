4367bbece7f8f40052cbcb387f0212577285ada0
Arturo Hermosa <keyblademaster_01@hotmail.com>
2022-06-29 18:00:27 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+basic.showString("Hello!")
+basic.forever(function () {
+	
+})
